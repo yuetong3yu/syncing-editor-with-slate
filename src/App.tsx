fef3dab31edaa3f16559d9c0fcd4c5e0b026534c
yuetong3yu <yuetong3yu@gmail.com>
2021-11-17 14:01:02 +0800

    ✨ update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
+import { useRef } from 'react'
 import './App.css'
-import { SyncingEditor } from './SyncingEditor'
+import { EditorProvider } from './SyncingEditor'
 
 const App = () => {
+  const oneRef = useRef<any>(null)
+
   return (
     <div className="webapp-container">
       <h2 style={{ marginBottom: 12 }}>See below:</h2>
-      <SyncingEditor />
+      <button
+        onClick={() => {
+          console.log(oneRef.current)
+          oneRef.current.editor.apply({
+            type: 'insert_text',
+            path: [0, 0],
+            offset: 0,
+            text: '1',
+          })
+        }}
+      >
+        see what happend
+      </button>
+      <EditorProvider ref={oneRef} />
       <hr
         style={{
           margin: '8px 0',
           backgroundColor: '#eee',
         }}
       />
-      <SyncingEditor />
+      <EditorProvider />
       <hr
         style={{
           margin: '8px 0',
--- src/SyncingEditor.tsx
+++ src/SyncingEditor.tsx
@@ -1,36 +1,30 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
-import { createEditor, Operation } from 'slate'
+import React, { useImperativeHandle, useMemo, useRef, useState } from 'react'
+import { createEditor } from 'slate'
 import { withReact, Slate, Editable } from 'slate-react'
 import { withHistory } from 'slate-history'
-import Mitt from 'mitt'
+// import Mitt from 'mitt'
 
 import { initialValue } from './constants'
 interface IProps {
   placeholder?: string
 }
 
-const mitt = Mitt()
+// const emiiter = Mitt()
 
-export const SyncingEditor: React.FC<IProps> = ({
-  placeholder = 'Please input here...',
-}) => {
+const SyncingEditor: React.FC<IProps> = (
+  { placeholder = 'Please input here...' },
+  ref
+) => {
   const [val, setVal] = useState(initialValue)
   const editor = useMemo(
     () => withHistory(withReact(createEditor() as any)),
     []
   )
-  const id = useRef(new Date().getTime())
-  const isFromRemote = useRef<boolean>(false)
+  const id = useRef(Date.now())
 
-  useEffect(() => {
-    ;(mitt as any).on('*', (type: string, val: Operation[]) => {
-      if (type !== String(id.current)) {
-        isFromRemote.current = true
-        val.forEach((op) => editor.apply(op))
-        isFromRemote.current = false
-      }
-    })
-  }, [])
+  useImperativeHandle(ref, () => ({
+    editor,
+  }))
 
   return (
     <div
@@ -41,20 +35,11 @@ export const SyncingEditor: React.FC<IProps> = ({
       }}
     >
       <Slate
+        key={id.current}
         editor={editor}
         value={val}
         onChange={(newval: any) => {
-          const ops = editor.operations
-            .filter(
-              (i: any) => i.type !== 'set_selection' && i.type !== 'set_value'
-            )
-            .map((o: any) => ({
-              ...o,
-              datasource: 'one',
-            }))
-          if (ops.length) {
-            mitt.emit(String(id.current), ops)
-          }
+          console.log(editor.operations)
           setVal(newval)
         }}
       >
@@ -63,3 +48,5 @@ export const SyncingEditor: React.FC<IProps> = ({
     </div>
   )
 }
+
+export const EditorProvider = React.forwardRef(SyncingEditor as any)
